77d8919a4e1652ede8395fed5f3723a386db12bd
SimCini <ciniltanisimone@gmail.com>
2025-04-17 10:31:07 +0200

    modifica prezzi

--- ciniltanilibrishop/ciniltanilibrishop/settings.py
+++ ciniltanilibrishop/ciniltanilibrishop/settings.py
@@ -132,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 AUTH_USER_MODEL = 'ecommerce.Utente'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
