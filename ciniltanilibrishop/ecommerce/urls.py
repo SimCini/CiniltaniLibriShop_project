6f9a3f676499b759b7b10adc6a11bacc3fa94d55
SimCini <ciniltanisimone@gmail.com>
2025-04-20 15:26:10 +0200

    .

--- ciniltanilibrishop/ecommerce/urls.py
+++ ciniltanilibrishop/ecommerce/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login',views.login, name='login'),
     path('registrati',views.registrati, name='registrati'),
     path('chi_sono',views.chi_sono, name='chi_sono'),
-    path('catalogo/', views.catalogo, name='catalogo_prodotti'),
+    path('catalogo/', views.catalogo, name='catalogo'),
     path('catalogo/<int:pk>/', views.dettaglio_prodotto, name='dettaglio_prodotto'),
     path('contatti/',views.contatti, name='contatti'),
 ]
