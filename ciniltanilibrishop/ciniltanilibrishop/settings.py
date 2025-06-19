a8548fbb25116d21edef49ade2d213cc67f39f4b
SimCini <ciniltanisimone@gmail.com>
2025-06-19 16:21:36 +0200

    modifice

--- ciniltanilibrishop/ciniltanilibrishop/settings.py
+++ ciniltanilibrishop/ciniltanilibrishop/settings.py
@@ -124,6 +124,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
